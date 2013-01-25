d8848455fa83686528f8f188b217e00315c493ff
Gustavo Gimenez <gimenezanderson@gmail.com>
2013-01-24 22:46:05 -0430

    Creando repositorio practica de Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tarea::Application.config.secret_token = '83595e60666edf013e49b2eb66db5e2a42d65dfc34be13f5711591fa3e1cee3c255923872e8c1ca47ea1e533b68e1b5ec2d8bfd51764a8e69fdd120f1d6808e9'
